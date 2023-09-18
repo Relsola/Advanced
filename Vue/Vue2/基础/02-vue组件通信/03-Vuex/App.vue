6ea58d1b8dc37d7f6d05169e2ec63e0438a408c0
Relsola <sola001018@gmail.com>
2023-09-18 20:29:57 +0800

    vue3基础

--- Vue/Vue2/基础/02-vue组件通信/03-Vuex/App.vue
+++ Vue/Vue2/基础/02-vue组件通信/03-Vuex/App.vue
@@ -3,6 +3,8 @@
     <h1>{{ $store.state.count }}</h1>
     <button @click="$store.commit('numAdd', 1)">+ 1</button>
     <button @click="$store.commit('numReduce', 1)">- 1</button>
+    <!-- 触发异步的 Action -->
+    <button @click="$store.commit('numAddAsync', 1)">- 1</button>
     <Demo />
   </div>
 </template>
